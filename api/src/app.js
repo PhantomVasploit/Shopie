8e1ffa02408270c166b68c27c69a3ae107c4ad0e
Paul-Sanga <paulsanganyamawi@gmail.com>
2023-08-18 09:40:17 +0300

    Project initialization

---
+++ api/src/app.js
@@ -0,0 +1,20 @@
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const express = require('express')
+const bodyParser = require('body-parser')
+require('dotenv').config()
+
+const app  = express()
+const port = process.env.PORT
+
+
+app.use(cors())
+app.use(helmet())
+app.use(morgan('tiny'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(port, ()=>{
+    console.log(`Shopie API server running on http://127.0.0.1:${port}/api/v1/`);
+})
