4cb943a8f998efc6a3c90ea5e0557356d74234bc
silomaben <kaybernard449@gmail.com>
2023-08-29 10:59:40 +0300

    Cypress e2e test

--- client/cypress/e2e/spec.cy.js
+++ client/cypress/e2e/spec.cy.js
@@ -1,76 +1,79 @@
 describe('customer use', () => {
   it("view specific category",()=>{
     cy.visit('http://localhost:5500/client/auth/html/login.html')
-    cy.get('#email').type('paulsanga@gmail.com');
+      cy.get('#email').type('paulsanga@gmail.com');
       cy.get('#password').type('12345678');
-    cy.get('#submit').click();
+  
+      cy.get('#submit').click();
+  
+      cy.url().should('include', '/client/customer/html/productCategories.html');
 
     cy.wait(1000)
     cy.get('#first-product').click({ force: true });
 
-    // to do
+    
 
     
     cy.url().should('include', 'client/customer/html/productCategories.html');
   })
 
-  // it("should add item to cart",()=>{
-  //   { multiple: true }
-  //   cy.visit('http://localhost:5500/client/auth/html/login.html')
-  //   cy.get('#email').type('paulsanga@gmail.com');
-  //   cy.get('#password').type('12345678');
-  //   cy.get('#submit').click();
+  it("should add item to cart",()=>{
+    { multiple: true }
+    cy.visit('http://localhost:5500/client/auth/html/login.html')
+    cy.get('#email').type('paulsanga@gmail.com');
+    cy.get('#password').type('12345678');
+    cy.get('#submit').click();
 
     
-  //   cy.visit('http://localhost:5500/client/customer/html/products.html?category=electronics')
-  //   cy.get('.btn').click();
+    cy.visit('http://localhost:5500/client/customer/html/products.html?category=electronics')
+    cy.get('.btn').click();
 
-  //   cy.get('.toastify', { timeout: 10000 }) 
-  //     .should('be.visible')
-  //     .contains('Item added to cart');
+    cy.get('.toastify', { timeout: 10000 }) 
+      .should('be.visible')
+      .contains('Item added to cart');
 
-  // })
-  // it("should add item to cart", () => {
-  //   cy.visit('http://localhost:5500/client/auth/html/login.html');
-  //   cy.get('#email').type('paulsanga@gmail.com');
-  //   cy.get('#password').type('12345678');
-  //   cy.get('#submit').click();
+  })
+  it("should add item to cart", () => {
+    cy.visit('http://localhost:5500/client/auth/html/login.html');
+    cy.get('#email').type('paulsanga@gmail.com');
+    cy.get('#password').type('12345678');
+    cy.get('#submit').click();
   
-  //   cy.url().should('include', 'client/customer/html/productCategories.html');
+    cy.url().should('include', 'client/customer/html/productCategories.html');
   
-  //   cy.visit('http://localhost:5500/client/customer/html/products.html?category=electronics');
+    cy.visit('http://localhost:5500/client/customer/html/products.html?category=electronics');
   
-  //   cy.url().should('include', '/client/customer/html/products.html');
+    cy.url().should('include', '/client/customer/html/products.html');
   
-  //   const addtocart = cy.get(".btn").eq(1).should("contain.text", "Add to cart");
+    const addtocart = cy.get(".btn").eq(1).should("contain.text", "Add to cart");
   
-  //   addtocart.click();
+    addtocart.click();
   
-  //   cy.get('.toastify', { timeout: 10000 })
-  //     .should('be.visible')
-  //     .contains('Item added to cart');
-  // });
+    cy.get('.toastify', { timeout: 10000 })
+      .should('be.visible')
+      .contains('Item added to cart');
+  });
   
 
   
 })
 
-// describe('cart checkout', () => {
+describe('cart checkout', () => {
   
-//   it("should checkout",()=>{
-//     cy.visit('http://localhost:5500/client/auth/html/login.html')
-//     cy.get('#email').type('paulsanga@gmail.com');
-//     cy.get('#password').type('12345678');
+  it("should checkout",()=>{
+    cy.visit('http://localhost:5500/client/auth/html/login.html')
+    cy.get('#email').type('paulsanga@gmail.com');
+    cy.get('#password').type('12345678');
 
-//     cy.get('#submit').click();
+    cy.get('#submit').click();
     
-//     cy.visit('http://localhost:5500/client/customer/html/viewCart.html')
+    cy.visit('http://localhost:5500/client/customer/html/viewCart.html')
   
-//     cy.get('#check-out').click();
+    cy.get('#check-out').click();
 
-//     cy.url().should('include', 'client/customer/html/productCategories.html');
+    cy.url().should('include', 'client/customer/html/productCategories.html');
 
-//   }) 
-// })
+  }) 
+})
 
 
